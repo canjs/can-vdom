ef27228bd164a42d369ce0af347ab873682e5ea7
Justin Meyer <justinbmeyer@gmail.com>
2016-05-16 16:23:18 -0500

    0.0.1

---
+++ dist/global/can-vdom.js
@@ -0,0 +1,126 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-vdom@0.0.0#make-window/make-window*/
+define('can-vdom/make-window/make-window', function (require, exports, module) {
+    var makeDocument = reuire('../make-document/make-document');
+    module.exports = function () {
+        var global = {};
+        global.document = makeDocument();
+        global.window = global;
+        global.addEventListener = function () {
+        };
+        global.removeEventListener = function () {
+        };
+        global.navigator = {
+            userAgent: '',
+            platform: '',
+            language: '',
+            languages: [],
+            plugins: [],
+            onLine: true
+        };
+        global.location = {
+            href: '',
+            protocol: '',
+            host: '',
+            hostname: '',
+            port: '',
+            pathname: '',
+            search: '',
+            hash: ''
+        };
+        global.history = {
+            pushState: can.k,
+            replaceState: can.k
+        };
+        return global;
+    };
+});
+/*can-vdom@0.0.0#can-vdom*/
+define('can-vdom', function (require, exports, module) {
+    var assign = require('can-util/js/assign/assign');
+    var makeWindow = require('can-vdom/make-window/make-window');
+    var GLOBAL = require('can-util/js/global/global');
+    var DOCUMENT = require('can-util/dom/document/document');
+    assign(GLOBAL(), makeWindow());
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
