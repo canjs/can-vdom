9979cfbb077e87bb907ac923cd840434e49db31d
Justin Meyer <justinbmeyer@gmail.com>
2016-05-16 21:26:26 -0500

    0.0.3

---
+++ dist/cjs/can-vdom.js
@@ -0,0 +1,6 @@
+/*can-vdom@0.0.2#can-vdom*/
+var assign = require('can-util/js/assign/assign');
+var makeWindow = require('./make-window/make-window.js');
+var GLOBAL = require('can-util/js/global/global');
+var DOCUMENT = require('can-util/dom/document/document');
+assign(GLOBAL(), makeWindow());
