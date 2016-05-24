a45ebd8a76804de0cc3a3c3365ff2908e722feb2
Matthew Phillips <matthew@matthewphillips.info>
2016-05-24 08:47:54 -0400

    0.0.4-0

---
+++ dist/amd/can-vdom.js
@@ -0,0 +1,8 @@
+/*can-vdom@0.0.3#can-vdom*/
+define(function (require, exports, module) {
+    var assign = require('can-util/js/assign');
+    var makeWindow = require('./make-window/make-window');
+    var GLOBAL = require('can-util/js/global');
+    var DOCUMENT = require('can-util/dom/document');
+    assign(GLOBAL(), makeWindow());
+});
