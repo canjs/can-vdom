92580a261d58612449ae53aab3a82b4204958370
John Gardner <john@ardea.io>
2016-10-17 14:50:02 -0500

    3.0.0-pre.1

---
+++ dist/cjs/can-vdom.js
@@ -0,0 +1,7 @@
+/*can-vdom@3.0.0-pre.0#can-vdom*/
+var assign = require('can-util/js/assign/assign');
+var makeWindow = require('./make-window/make-window.js');
+var GLOBAL = require('can-util/js/global/global');
+var DOCUMENT = require('can-util/dom/document/document');
+var global = GLOBAL();
+assign(global, makeWindow(global));
