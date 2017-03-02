14be653566872af63f9e37fd6166852a45141724
Marshall Thompson <marshall@creativeideal.net>
2017-03-02 16:33:28 -0700

    3.0.2

---
+++ dist/amd/can-vdom.js
@@ -0,0 +1,13 @@
+/*can-vdom@3.0.1#can-vdom*/
+define(function (require, exports, module) {
+    (function (global) {
+        var assign = require('can-util/js/assign');
+        var makeWindow = require('./make-window/make-window');
+        var GLOBAL = require('can-util/js/global');
+        var DOCUMENT = require('can-util/dom/document');
+        var global = GLOBAL();
+        assign(global, makeWindow(global));
+    }(function () {
+        return this;
+    }()));
+});
