f40bb99e3c9747e81a45499f2b54f4e7beae5622
Matthew Phillips <matthew@matthewphillips.info>
2017-07-10 09:29:00 -0400

    3.1.1

---
+++ dist/cjs/make-window/make-window.js
@@ -0,0 +1,36 @@
+/*can-vdom@3.1.0#make-window/make-window*/
+var makeDocument = require('../make-document/make-document.js');
+var noop = function () {
+};
+module.exports = function (global) {
+    global = global || {};
+    global.document = makeDocument();
+    global.window = global.self = global;
+    global.addEventListener = function () {
+    };
+    global.removeEventListener = function () {
+    };
+    global.navigator = {
+        userAgent: '',
+        platform: '',
+        language: '',
+        languages: [],
+        plugins: [],
+        onLine: true
+    };
+    global.location = {
+        href: '',
+        protocol: '',
+        host: '',
+        hostname: '',
+        port: '',
+        pathname: '',
+        search: '',
+        hash: ''
+    };
+    global.history = {
+        pushState: noop,
+        replaceState: noop
+    };
+    return global;
+};
