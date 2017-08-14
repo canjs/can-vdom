b025a74651d5c95219b7d7381ce005f71d5ac55d
Matthew Phillips <matthew@matthewphillips.info>
2017-08-14 08:24:16 -0400

    3.2.0

---
+++ dist/cjs/make-window/make-window.js
@@ -0,0 +1,38 @@
+/*can-vdom@3.1.1#make-window/make-window*/
+var makeDocument = require('../make-document/make-document.js');
+var simpleDOM = require('can-simple-dom');
+var noop = function () {
+};
+module.exports = function (global) {
+    global = global || {};
+    global.document = makeDocument();
+    global.window = global.self = global;
+    global.addEventListener = function () {
+    };
+    global.removeEventListener = function () {
+    };
+    global.Node = simpleDOM.Node;
+    global.navigator = {
+        userAgent: '',
+        platform: '',
+        language: '',
+        languages: [],
+        plugins: [],
+        onLine: true
+    };
+    global.location = {
+        href: '',
+        protocol: '',
+        host: '',
+        hostname: '',
+        port: '',
+        pathname: '',
+        search: '',
+        hash: ''
+    };
+    global.history = {
+        pushState: noop,
+        replaceState: noop
+    };
+    return global;
+};
