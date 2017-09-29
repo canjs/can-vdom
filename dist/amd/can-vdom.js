d8e958b2ecdd48e69a37ce9495ef38edc7c01af9
imasustink <austin@austinkurpuis.com>
2017-09-28 19:51:49 -0700

    3.2.1

---
+++ dist/amd/can-vdom.js
@@ -0,0 +1,19 @@
+/*can-vdom@3.2.0#can-vdom*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-assign',
+    './make-window/make-window',
+    'can-globals/global'
+], function (require, exports, module) {
+    (function (global) {
+        var assign = require('can-assign');
+        var makeWindow = require('./make-window/make-window');
+        var GLOBAL = require('can-globals/global');
+        var global = GLOBAL();
+        assign(global, makeWindow(global));
+    }(function () {
+        return this;
+    }()));
+});
---
+++ dist/cjs/can-vdom.js
@@ -0,0 +1,6 @@
+/*can-vdom@3.2.0#can-vdom*/
+var assign = require('can-assign');
+var makeWindow = require('./make-window/make-window.js');
+var GLOBAL = require('can-globals/global/global');
+var global = GLOBAL();
+assign(global, makeWindow(global));
