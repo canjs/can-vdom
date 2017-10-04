c761eaf615f595af8a3c048425d228c753e83c0c
Bradley Momberger <brad@bitovi.com>
2017-10-04 15:22:48 -0400

    3.2.3

---
+++ dist/amd/make-document/make-document.js
@@ -0,0 +1,22 @@
+/*can-vdom@3.2.2#make-document/make-document*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-simple-dom',
+    '../make-parser/make-parser'
+], function (require, exports, module) {
+    var simpleDOM = require('can-simple-dom');
+    var makeParser = require('../make-parser/make-parser');
+    function CanSimpleDocument() {
+        simpleDOM.Document.apply(this, arguments);
+        var serializer = new simpleDOM.HTMLSerializer(simpleDOM.voidMap);
+        var parser = makeParser(this);
+        this.__addSerializerAndParser(serializer, parser);
+    }
+    CanSimpleDocument.prototype = new simpleDOM.Document();
+    CanSimpleDocument.prototype.constructor = CanSimpleDocument;
+    module.exports = function () {
+        return new CanSimpleDocument();
+    };
+});
