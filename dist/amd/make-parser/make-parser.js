a9dc25b7000ab81d8cd22486dbb68b7f9446a97f
imasustink <austin@austinkurpuis.com>
2017-10-31 19:44:47 -0700

    3.2.4

---
+++ dist/amd/make-parser/make-parser.js
@@ -0,0 +1,65 @@
+/*can-vdom@3.2.3#make-parser/make-parser*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-view-parser',
+    'can-simple-dom'
+], function (require, exports, module) {
+    var canParser = require('can-view-parser');
+    var simpleDOM = require('can-simple-dom');
+    module.exports = function (document) {
+        return new simpleDOM.HTMLParser(function (string) {
+            var tokens = [];
+            var currentTag, currentAttr;
+            canParser(string, {
+                start: function (tagName, unary) {
+                    currentTag = {
+                        type: 'StartTag',
+                        attributes: [],
+                        tagName: tagName
+                    };
+                },
+                end: function (tagName, unary) {
+                    tokens.push(currentTag);
+                    currentTag = undefined;
+                },
+                close: function (tagName) {
+                    tokens.push({
+                        type: 'EndTag',
+                        tagName: tagName
+                    });
+                },
+                attrStart: function (attrName) {
+                    currentAttr = [
+                        attrName,
+                        ''
+                    ];
+                    currentTag.attributes.push(currentAttr);
+                },
+                attrEnd: function (attrName) {
+                },
+                attrValue: function (value) {
+                    currentAttr[1] += value;
+                },
+                chars: function (value) {
+                    tokens.push({
+                        type: 'Chars',
+                        chars: value
+                    });
+                },
+                comment: function (value) {
+                    tokens.push({
+                        type: 'Comment',
+                        chars: value
+                    });
+                },
+                special: function (value) {
+                },
+                done: function () {
+                }
+            });
+            return tokens;
+        }, document, simpleDOM.voidMap);
+    };
+});
