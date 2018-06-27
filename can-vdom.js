a7708f214cfb209875a0f2586e5dbd80b475f9ee
Austin Sanders <austinsanders@Austins-MacBook-Pro.local>
2018-06-27 15:17:40 -0700

    Adds use strict. Fixes canjs/canjs#4217

--- can-vdom.js
+++ can-vdom.js
@@ -1,3 +1,4 @@
+"use strict";
 var assign = require("can-assign");
 var makeWindow = require("./make-window/make-window");
 
--- make-document/make-document.js
+++ make-document/make-document.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * @module {function} can-vdom/make-document/make-document ./make-document/make-document
  * @parent can-vdom.modules
