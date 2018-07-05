f1d81bd0e2cb2327e666663028292437858604ce
Chasen Le Hara <chasen@chasenlehara.com>
2018-07-05 09:47:23 -0700

    4.2.0

---
+++ dist/cjs/make-document/make-document.js
@@ -0,0 +1,15 @@
+/*can-vdom@4.1.0#make-document/make-document*/
+'use strict';
+var simpleDOM = require('can-simple-dom');
+var makeParser = require('../make-parser/make-parser.js');
+function CanSimpleDocument() {
+    simpleDOM.Document.apply(this, arguments);
+    var serializer = new simpleDOM.HTMLSerializer(simpleDOM.voidMap);
+    var parser = makeParser(this);
+    this.__addSerializerAndParser(serializer, parser);
+}
+CanSimpleDocument.prototype = new simpleDOM.Document();
+CanSimpleDocument.prototype.constructor = CanSimpleDocument;
+module.exports = function () {
+    return new CanSimpleDocument();
+};
