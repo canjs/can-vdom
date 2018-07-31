93bf45c595933c58d14ab88e6d8e044e96d5ae66
Matthew Phillips <matthew@matthewphillips.info>
2018-07-31 16:37:23 -0400

    4.3.0

---
+++ dist/amd/make-document/make-document.js
@@ -0,0 +1,23 @@
+/*can-vdom@4.2.0#make-document/make-document*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-simple-dom',
+    '../make-parser/make-parser'
+], function (require, exports, module) {
+    'use strict';
+    var simpleDOM = require('can-simple-dom');
+    var makeParser = require('../make-parser/make-parser');
+    function CanSimpleDocument() {
+        simpleDOM.Document.apply(this, arguments);
+        var serializer = new simpleDOM.HTMLSerializer(simpleDOM.voidMap);
+        var parser = makeParser(this);
+        this.__addSerializerAndParser(serializer, parser);
+    }
+    CanSimpleDocument.prototype = new simpleDOM.Document();
+    CanSimpleDocument.prototype.constructor = CanSimpleDocument;
+    module.exports = function () {
+        return new CanSimpleDocument();
+    };
+});
---
+++ dist/amd/make-window/make-window.js
@@ -0,0 +1,74 @@
+/*can-vdom@4.2.0#make-window/make-window*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../make-document/make-document',
+    'can-simple-dom',
+    'can-log/dev'
+], function (require, exports, module) {
+    (function (global, require, exports, module) {
+        'use strict';
+        var makeDocument = require('../make-document/make-document');
+        var simpleDOM = require('can-simple-dom');
+        var devLog = require('can-log/dev');
+        var noop = function () {
+        };
+        function warnOnAccess(global, property) {
+            var hasWarned = false;
+            var value = global[property];
+            Object.defineProperty(global, property, {
+                get: function () {
+                    if (!hasWarned) {
+                        hasWarned = true;
+                        devLog.warn('The global ' + property + ' is not supported by can-vdom.');
+                    }
+                    return value;
+                },
+                set: function (newValue) {
+                    value = newValue;
+                }
+            });
+        }
+        module.exports = function (global) {
+            global = global || {};
+            global.document = makeDocument();
+            global.window = global.self = global;
+            global.addEventListener = function () {
+            };
+            global.removeEventListener = function () {
+            };
+            global.Node = simpleDOM.Node;
+            global.Element = simpleDOM.Element;
+            global.navigator = {
+                userAgent: '',
+                platform: '',
+                language: '',
+                languages: [],
+                plugins: [],
+                onLine: true
+            };
+            global.location = {
+                href: '',
+                protocol: '',
+                host: '',
+                hostname: '',
+                port: '',
+                pathname: '',
+                search: '',
+                hash: ''
+            };
+            global.history = {
+                pushState: noop,
+                replaceState: noop
+            };
+            global.getComputedStyle = function (node) {
+                return node.style;
+            };
+            warnOnAccess(global, 'FormData');
+            return global;
+        };
+    }(function () {
+        return this;
+    }(), require, exports, module));
+});
