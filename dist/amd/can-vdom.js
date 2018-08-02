855619619bd65fa223769ed601a68522953d8560
Matthew Phillips <matthew@matthewphillips.info>
2018-08-02 16:06:17 -0400

    4.4.0

---
+++ dist/amd/can-vdom.js
@@ -0,0 +1,20 @@
+/*can-vdom@4.3.0#can-vdom*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-assign',
+    './make-window/make-window',
+    'can-globals/global'
+], function (require, exports, module) {
+    (function (global, require, exports, module) {
+        'use strict';
+        var assign = require('can-assign');
+        var makeWindow = require('./make-window/make-window');
+        var GLOBAL = require('can-globals/global');
+        var global = GLOBAL();
+        assign(global, makeWindow(global));
+    }(function () {
+        return this;
+    }(), require, exports, module));
+});
---
+++ dist/cjs/make-window/make-window.js
@@ -0,0 +1,61 @@
+/*can-vdom@4.3.0#make-window/make-window*/
+'use strict';
+var makeDocument = require('../make-document/make-document.js');
+var simpleDOM = require('can-simple-dom');
+var devLog = require('can-log/dev/dev');
+var noop = function () {
+};
+function warnOnAccess(global, property) {
+    var hasWarned = false;
+    var value = global[property];
+    Object.defineProperty(global, property, {
+        get: function () {
+            if (!hasWarned) {
+                hasWarned = true;
+                devLog.warn('The global ' + property + ' is not supported by can-vdom.');
+            }
+            return value;
+        },
+        set: function (newValue) {
+            value = newValue;
+        }
+    });
+}
+module.exports = function (global) {
+    global = global || {};
+    global.document = makeDocument();
+    global.window = global.self = global.document.defaultView = global;
+    global.addEventListener = function () {
+    };
+    global.removeEventListener = function () {
+    };
+    global.Node = simpleDOM.Node;
+    global.Element = simpleDOM.Element;
+    global.navigator = {
+        userAgent: '',
+        platform: '',
+        language: '',
+        languages: [],
+        plugins: [],
+        onLine: true
+    };
+    global.location = {
+        href: '',
+        protocol: '',
+        host: '',
+        hostname: '',
+        port: '',
+        pathname: '',
+        search: '',
+        hash: ''
+    };
+    global.history = {
+        pushState: noop,
+        replaceState: noop
+    };
+    global.getComputedStyle = function (node) {
+        return node.style;
+    };
+    warnOnAccess(global, 'FormData');
+    return global;
+};
