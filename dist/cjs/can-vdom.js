336a5dac9995a31b982e349e0c3043533d97e6e2
Matthew Phillips <matthew@matthewphillips.info>
2018-12-27 09:22:26 -0500

    4.4.1

---
+++ dist/cjs/can-vdom.js
@@ -0,0 +1,7 @@
+/*can-vdom@4.4.0#can-vdom*/
+'use strict';
+var assign = require('can-assign');
+var makeWindow = require('./make-window/make-window.js');
+var GLOBAL = require('can-globals/global/global');
+var global = GLOBAL();
+assign(global, makeWindow(global));
